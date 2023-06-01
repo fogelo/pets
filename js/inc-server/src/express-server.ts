c2832f0451ed4330cea3ad70be66cbd740c6e51e
fogelo <fogelo@yandex.ru>
2023-06-01 17:11:49 +0300

    минимальная настройка окружения

---
+++ js/inc-server/src/express-server.ts
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello worldd");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Home page");
+});
+
+app.listen(port, () => {
+  console.log("Server is working");
+});
