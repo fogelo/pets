b506efcdf0c229fd132e17f8af12db8bc83cb306
fogelo <fogelo@yandex.ru>
2023-06-01 11:34:20 +0300

    простая имитация задержки

--- js/inc-server/server.js
+++ js/inc-server/server.js
@@ -1,9 +1,26 @@
 const http = require("http");
 
-
 const server = http.createServer((request, response) => {
-  response.write("response anton");
-  response.end();
+  switch (request.url) {
+    case "/home": {
+      const start = new Date();
+      while (new Date() - start < 5000) {
+        console.log(new Date() - start < 5000);
+      }
+      response.write("home page");
+      response.end();
+      break;
+    }
+    case "/about": {
+      response.write("about page");
+      response.end();
+      break;
+    }
+    default: {
+      response.write("404 not found");
+      response.end();
+    }
+  }
 });
 
 server.listen(3003);
