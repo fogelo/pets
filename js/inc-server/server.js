2f957a676b76d465a027f8da1c66faf839252962
fogelo <fogelo@yandex.ru>
2023-06-01 10:17:47 +0300

    первый сервер

--- js/inc-server/server.js
+++ js/inc-server/server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
+
 const server = http.createServer((request, response) => {
-  response.write("ответ по url anton");
+  response.write("response anton");
   response.end();
 });
 
