42439c66dd00657449223a7765e49d116dcdbade
fogelo <fogelo@yandex.ru>
2023-08-23 13:42:47 +0300

    brush по кнопке

--- js/ulb/paint/src/components/Canvas.tsx
+++ js/ulb/paint/src/components/Canvas.tsx
@@ -2,15 +2,14 @@ import { observer } from "mobx-react-lite";
 import "../styles/canvas.scss";
 import { useEffect, useRef } from "react";
 import canvasState from "../store/canvasState";
-import toolState from "../store/toolState";
-import Brush from "../tools/Brush";
 
 const Canvas = observer(() => {
   const canvasRef = useRef(null);
   useEffect(() => {
     canvasState.setCanvas(canvasRef.current);
-    toolState.setTool(new Brush(canvasRef.current))
+    
   }, []);
+
   return (
     <div className="canvas">
       <canvas ref={canvasRef} width={600} height={400} />
--- js/ulb/paint/src/components/Toolbar.tsx
+++ js/ulb/paint/src/components/Toolbar.tsx
@@ -1,34 +1,26 @@
-import "../styles/toolbar.scss"
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
+import "../styles/toolbar.scss";
+import Brush from "../tools/Brush";
 const Toolbar = () => {
   return (
     <div className="toolbar">
-      <button className="toolbar__btn brush">
+      <button
+        className="toolbar__btn brush"
+        onClick={() => toolState.setTool(new Brush(canvasState.canvas))}
+      >
         brush
       </button>
-      <button className="toolbar__btn rect">
-       rect 
-      </button>
-      <button className="toolbar__btn circle">
-        circle
-      </button>
-      <button className="toolbar__btn eraser">
-        eraser
-      </button>
-      <button className="toolbar__btn line">
-        line
-      </button>
+      <button className="toolbar__btn rect">rect</button>
+      <button className="toolbar__btn circle">circle</button>
+      <button className="toolbar__btn eraser">eraser</button>
+      <button className="toolbar__btn line">line</button>
       <input type="color" />
-      <button className="toolbar__btn undo">
-        undo
-      </button>
-      <button className="toolbar__btn redo">
-        redo
-      </button>
-      <button className="toolbar__btn save">
-        save
-      </button>
+      <button className="toolbar__btn undo">undo</button>
+      <button className="toolbar__btn redo">redo</button>
+      <button className="toolbar__btn save">save</button>
     </div>
-  )
-}
+  );
+};
 
-export default Toolbar
+export default Toolbar;
