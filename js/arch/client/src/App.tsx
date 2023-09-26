98c2cdef380ecbb68b6da9d202e95101f7bb5c1b
fogelo <fogelo@yandex.ru>
2023-09-26 10:05:16 +0300

    тестирвоание react-pdf

---
+++ js/arch/client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.scss";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Button> helloasfsd</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ js/arch/client/src/components/Button/index.tsx
@@ -0,0 +1,6 @@
+import { Button as BaseButton } from "antd";
+
+const Button = (props) => {
+  return <BaseButton {...props} />;
+};
+export default Button;
