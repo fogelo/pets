e62a40581032368996431913d6d07cec8c71add5
fogelo <fogelo@yandex.ru>
2023-10-27 22:26:42 +0300

    для дерева

---
+++ js/diff/svg/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <svg width="2000" height="1000">
+      <rect x="10" y="20" width="180" height="60" fill="blue" />
+
+      <text dominantBaseline="hanging">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod eum
+        nesciunt recusandae iste sed voluptatum maiores asperiores, dolores
+        commodi quis quisquam hic et sapiente quaerat deleniti modi! Molestiae
+        cumque, obcaecati maxime quam ut asperiores repellat corporis nisi vero,
+        officia minima dolorum illum ullam aliquid id neque natus. Inventore,
+        expedita non. Dolor officia, repellat odit, ipsa ipsam doloribus autem
+        est nihil impedit quas vitae. Porro assumenda cupiditate eos incidunt
+        quia est architecto odio explicabo voluptates quam qui illum, optio amet
+        vitae voluptatem autem consequuntur at? Excepturi ut tempore id, eos
+        suscipit veniam consequuntur ipsam, commodi deserunt voluptates sit,
+        ratione maxime impedit!
+      </text>
+    </svg>
+  );
+}
+
+export default App;
