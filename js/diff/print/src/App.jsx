12f3c451cd275f9d891eb1864a7cc6702399b493
fogelo <fogelo@yandex.ru>
2023-12-01 10:45:15 +0300

    немного

--- js/diff/print/src/App.jsx
+++ js/diff/print/src/App.jsx
@@ -9,8 +9,34 @@ function App() {
     <>
       {/* <Print /> */}
       {/* <PDF /> */}
-      <A />
-      <B />
+      {/* <A /> */}
+      {/* <B /> */}
+      <div className="div">
+        <table>
+          <thead>
+            <tr>
+              <td>111</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                  222 lorem100 Start editing to see some magic happenStart
+                  editing to see some magic happenStart editing to see some
+                  magic happenStart editing to see some magic happen 222
+                  lorem100 Start editing to see some magic happenStart editing
+                  to see some magic happenStart editing to see some magic
+                  happenStart editing to see some magic happen
+
+              </td>
+              {/* <td>222</td> */}
+            </tr>
+            <tr>
+              <td>333</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
