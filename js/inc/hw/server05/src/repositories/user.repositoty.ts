64edc3388c1fb49ec0d6e88de33fe15a8a2aafdf
fogelo <fogelo@yandex.ru>
2024-04-06 22:06:01 +0300

    feat: переработан API для пропса color компонента Button
    Теперь что-то там
    
    breaking change: что-то больше не поддерживается

--- js/inc/hw/server05/src/repositories/user.repositoty.ts
+++ js/inc/hw/server05/src/repositories/user.repositoty.ts
@@ -30,6 +30,7 @@ export class UserRepository {
       filters.push({ login: { $regex: searchLoginTerm, $options: "i" } });
     }
 
+
     
     if (filters.length > 0) {
       filter.$or = filters;
