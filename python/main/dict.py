2788729f0eb6541afc7242d2b4f4c09d335f608e
fogelo <fogelo@yandex.ru>
2024-07-01 09:17:55 +0300

    loops начало

---
+++ python/main/dict.py
@@ -0,0 +1,9 @@
+"""  """
+my_dict = {"name": "Anton", "surname": "Orlov", "age": 33, "married": True}
+
+# * Методы обычные
+
+my_dict_copy = my_dict.copy() # создает поверхностную копию
+#my_dict.clear() # очищает словарь
+
+
