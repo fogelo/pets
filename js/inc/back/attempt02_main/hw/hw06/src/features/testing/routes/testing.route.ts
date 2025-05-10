94fae55ad881d2d3c9bd7dad98ea8997d19aa5fb
anton <fogelo@yandex.ru>
2025-05-11 02:33:26 +0300

    hw06 - добавил роутер для тестирования

---
+++ js/inc/back/attempt02_main/hw/hw06/src/features/testing/routes/testing.route.ts
@@ -0,0 +1,19 @@
+import { Router, Request, Response } from "express";
+import {
+  blogsCollection,
+  postsCollection,
+  usersCollection,
+} from "../../../db/db";
+import { HttpStatus } from "../../../core/types/http-statuses";
+
+export const testingRouter: Router = Router({});
+
+testingRouter.delete("/all-data", async (req: Request, res: Response) => {
+  //truncate db
+  await Promise.all([
+    usersCollection.deleteMany(),
+    blogsCollection.deleteMany(),
+    postsCollection.deleteMany(),
+  ]);
+  res.sendStatus(HttpStatus.NoContent);
+});
--- js/inc/back/attempt02_main/hw/hw06/src/set-app.ts
+++ js/inc/back/attempt02_main/hw/hw06/src/set-app.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { BLOGS_PATH, MAIN_PATH, POSTS_PATH, USERS_PATH } from "./core/paths/paths";
+import { BLOGS_PATH, MAIN_PATH, POSTS_PATH, TESTING_PATH, USERS_PATH } from "./core/paths/paths";
 import { blogsRouter } from "./features/blogs/routes/blogs.route";
 import { postsRouter } from "./features/posts/routes/posts.route";
 import { usersRouter } from "./features/users/routes/users.route";
+import { testingRouter } from "./features/testing/routes/testing.route";
 
 export const setApp = (app: express.Express) => {
   app.use(express.json());
@@ -10,6 +11,7 @@ export const setApp = (app: express.Express) => {
   app.use(USERS_PATH, usersRouter);
   app.use(BLOGS_PATH, blogsRouter);
   app.use(POSTS_PATH, postsRouter);
+  app.use(TESTING_PATH, testingRouter);
 
   app.get(`${MAIN_PATH}`, (req: express.Request, res: express.Response) => {
     res.send("Hello Worldd!");
