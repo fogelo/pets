f23bcd250d3ff1a9f05822d231e582be2109bef8
anton <fogelo@yandex.ru>
2025-05-17 00:49:52 +0300

    hw09 - правка

--- js/inc/back/attempt02_main/hw/hw09/src/features/auth/routes/devices.route.ts
+++ js/inc/back/attempt02_main/hw/hw09/src/features/auth/routes/devices.route.ts
@@ -3,10 +3,11 @@ import { authMiddleware } from "../auth.middleware";
 import { getUserDeviceListHandler } from "./handlers/get-device-list.handler";
 import { deleteAllUserDevicesHandler } from "./handlers/delete.all-user.devices.handler";
 import { deleteDeviceHandler } from "./handlers/delete.device.handler";
+import { idValidation } from "../../../core/middlewares/params-id.validation-middleware";
 
 export const devicesRouter: Router = Router({});
 
 devicesRouter
   .get("", authMiddleware, getUserDeviceListHandler)
   .delete("", authMiddleware, deleteAllUserDevicesHandler)
-  .delete("/:id", authMiddleware, deleteDeviceHandler);
+  .delete("/:id", authMiddleware, idValidation, deleteDeviceHandler);
