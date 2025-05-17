603a7e74aae0445b84b57b3638243dd6ac7e3ea3
anton <fogelo@yandex.ru>
2025-05-17 22:30:57 +0300

    hw09 - try/catch

--- js/inc/back/attempt02_main/hw/hw09/src/features/auth/routes/handlers/refresh.token.handler.ts
+++ js/inc/back/attempt02_main/hw/hw09/src/features/auth/routes/handlers/refresh.token.handler.ts
@@ -5,67 +5,72 @@ import { userRepository } from "../../../users/repositories/users.repository";
 import { devicesService } from "../../application/devices.service";
 
 export const refreshTokenHandler = async (req: Request, res: Response) => {
-  const refreshToken = req.cookies.refreshToken;
-  if (!refreshToken) {
-    res.sendStatus(HttpStatus.Unauthorized);
-    return;
-  }
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      res.sendStatus(HttpStatus.Unauthorized);
+      return;
+    }
 
-  // 1) Проверяем, не в чёрном списке ли токен
-  const revokedRefreshToken = await jwtService.findRefreshTokenInBlackList(
-    refreshToken
-  );
-  if (revokedRefreshToken) {
-    res.sendStatus(HttpStatus.Unauthorized);
-    return;
-  }
+    // 1) Проверяем, не в чёрном списке ли токен
+    const revokedRefreshToken = await jwtService.findRefreshTokenInBlackList(
+      refreshToken
+    );
+    if (revokedRefreshToken) {
+      res.sendStatus(HttpStatus.Unauthorized);
+      return;
+    }
 
-  // 2) Верифицируем токен
-  const decoded = await jwtService.verifyRefreshToken(refreshToken);
-  if (!decoded?.userId) {
-    res.sendStatus(HttpStatus.Unauthorized);
-    return;
-  }
+    // 2) Верифицируем токен
+    const decoded = await jwtService.verifyRefreshToken(refreshToken);
+    if (!decoded?.userId) {
+      res.sendStatus(HttpStatus.Unauthorized);
+      return;
+    }
 
-  // 3) **Проверяем, что сессия ещё жива**
-  const session = await devicesService.findByDeviceId(decoded.deviceId);
-  if (!session) {
-    // после удаления /security/devices/:deviceId этого deviceId больше нет
-    res.sendStatus(HttpStatus.Unauthorized);
-    return 
-  }
+    // 3) **Проверяем, что сессия ещё жива**
+    const session = await devicesService.findByDeviceId(decoded.deviceId);
+    if (!session) {
+      // после удаления /security/devices/:deviceId этого deviceId больше нет
+      res.sendStatus(HttpStatus.Unauthorized);
+      return;
+    }
 
-  //добавляем старый токен в черный список
-  await jwtService.addRefreshTokenToBlackList(refreshToken);
+    //добавляем старый токен в черный список
+    await jwtService.addRefreshTokenToBlackList(refreshToken);
 
-  const user = await userRepository.findByIdOrFail(decoded.userId);
-  // создаем новую пару токенов
-  const newAccessToken = await jwtService.createAccessToken(
-    user,
-    decoded.deviceId
-  );
-  const newRefreshToken = await jwtService.createRefreshToken(
-    user,
-    decoded.deviceId
-  );
+    const user = await userRepository.findByIdOrFail(decoded.userId);
+    // создаем новую пару токенов
+    const newAccessToken = await jwtService.createAccessToken(
+      user,
+      decoded.deviceId
+    );
+    const newRefreshToken = await jwtService.createRefreshToken(
+      user,
+      decoded.deviceId
+    );
 
-  //обновляем девайс (сессию)
-  const newRefresTokenDecoded = await jwtService.verifyRefreshToken(
-    newRefreshToken
-  );
-  if (newRefresTokenDecoded) {
-    await devicesService.updateByDeviceId(newRefresTokenDecoded.deviceId, {
-      ...newRefresTokenDecoded,
-      lastActiveDate: new Date(),
+    //обновляем девайс (сессию)
+    const newRefresTokenDecoded = await jwtService.verifyRefreshToken(
+      newRefreshToken
+    );
+    if (newRefresTokenDecoded) {
+      await devicesService.updateByDeviceId(newRefresTokenDecoded.deviceId, {
+        ...newRefresTokenDecoded,
+        lastActiveDate: new Date(),
+      });
+    }
+
+    // обновляем куку
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: true,
     });
+
+    res.status(HttpStatus.Ok).json({ accessToken: newAccessToken });
+    return;
+  } catch (err) {
+    res.sendStatus(HttpStatus.Unauthorized);
+    return;
   }
-
-  // обновляем куку
-  res.cookie("refreshToken", newRefreshToken, {
-    httpOnly: true,
-    secure: true,
-  });
-
-  res.status(HttpStatus.Ok).json({ accessToken: newAccessToken });
-  return;
 };
