3ab2df9743c6216ff7ab501e5a7fafdc7799799d
anton <fogelo@yandex.ru>
2025-06-02 01:33:29 +0300

    hw13 - + тест

--- js/inc/back/attempt02_main/hw/hw13/src/setup/pipes.setup.ts
+++ js/inc/back/attempt02_main/hw/hw13/src/setup/pipes.setup.ts
@@ -10,8 +10,8 @@ export const pipesSetup = (app: INestApplication) => {
   app.useGlobalPipes(
     new ValidationPipe({
       //выбросит ошибку если в теле запроса укажут лишние поля
-      whitelist: true, // Удаляет лишние поля
-      forbidNonWhitelisted: true, // Выбрасывает ошибку, если есть лишние поля
+      // whitelist: true, // Удаляет лишние поля
+      // forbidNonWhitelisted: true, // Выбрасывает ошибку, если есть лишние поля
       //-------
       stopAtFirstError: true, // по одному полю передаем только одну ошибку (первую)
       transform: true, // трансформирует тело запрос, query и uri параметри в указанные типы, createUser(@Body() dto: CreateUserInputDto) - dto будет не просто Object, а CreateUserInputDto
