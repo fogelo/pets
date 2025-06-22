f9a3b4a9dd36481059dd5061336a1b5d891dbf7d
anton <fogelo@yandex.ru>
2025-06-23 00:35:18 +0300

    Revert "hw14 - правка"
    This reverts commit f37dab72326c37cf062e7ee7504cea72862039e4.

--- js/inc/back/attempt02_main/hw/hw14/src/modules/user-accounts/domain/user.entity.ts
+++ js/inc/back/attempt02_main/hw/hw14/src/modules/user-accounts/domain/user.entity.ts
@@ -90,7 +90,6 @@ export class User {
   }
   setConfirmationCode(confirmationCode: string) {
     this.confirmationCode = confirmationCode;
-    this.isEmailConfirmed = false;
   }
 
   setPasswordRecoveryCode(recoveryCode: string, expiresAt: Date) {
