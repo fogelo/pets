3885647340578cbf2a12ff250253274a65e6c6e8
anton <fogelo@yandex.ru>
2025-06-22 23:33:56 +0300

    Revert "hw14 - убраны лишние ошибки"
    This reverts commit 40eb7f9d7f7b9a4f7bb1d13a86bade18c13e6e33.

--- js/inc/back/attempt02_main/hw/hw14/src/modules/user-accounts/application/users.service.ts
+++ js/inc/back/attempt02_main/hw/hw14/src/modules/user-accounts/application/users.service.ts
@@ -91,9 +91,22 @@ export class UsersService {
   async resendRegistrationEmail(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
-    if (!user || user.isEmailConfirmed) {
-      // Не выбрасываем ошибку, чтобы не раскрывать, существует ли пользователь или подтвержден ли его email
-      return;
+    if (!user) {
+      throw new BadRequestException([
+        {
+          message: 'User with this email does not exist',
+          field: 'email',
+        },
+      ]);
+    }
+
+    if (user.isEmailConfirmed) {
+      throw new BadRequestException([
+        {
+          message: 'Email is already confirmed',
+          field: 'email',
+        },
+      ]);
     }
 
     // Генерируем новый код подтверждения
