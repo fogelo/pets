3237e76dad185f37b713d2ff64d77c623a1ab075
anton <fogelo@yandex.ru>
2025-09-23 16:37:31 +0300

    k8s

---
+++ devops/k8s/bgdn/k8s-web-hello/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  const helloMessage = `<h1>VERSION 1: Hello from the ${os.hostname()}</h1>`;
+  console.log(helloMessage);
+  res.send(helloMessage);
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server is listening at port ${PORT}`);
+});
